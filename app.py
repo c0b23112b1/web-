58f5ced2dbe098070ee047660134cb9ff300efc6
Nita daiki <c0b23112b1@edu.teu.ac.jp>
2025-11-10 01:01:43 +0900

    Remove tracked virtual environment and update .gitignore

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
 from flask import Flask, render_template, request, jsonify
+import psycopg2
+
+def get_connection():
+    try:
+        con = psycopg2.connect(
+            host="localhost",       # 接続先ホスト
+            port=5432,              # PostgreSQLのデフォルトポート
+            database="mydb",        # データベース名
+            user="postgres",          # ユーザー名
+            password="yourpassword"   # パスワード
+        )
+        print("接続に成功しました")
+        return con
+    except Exception as e:
+        print("接続に失敗しました:", e)
+        return None
 
 app = Flask(__name__)
+con = get_connection()
+
+
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('menu.html')
+
+@app.route("/main")
+def main():
+    return render_template('main.html')
 
 @app.route('/schedule')
 def schedule():
